aaacc26cc12096038312c6d3068bc457ffac5573
Hiroki Kato <yuyuqq@gmail.com>
2025-03-16 17:08:24 +0900

    attitude control RK4 implemented

--- space_station_gnc/src/attitude_dynamics.cpp
+++ space_station_gnc/src/attitude_dynamics.cpp
@@ -12,6 +12,7 @@
 #include <std_msgs/msg/float64.hpp>
 #include <chrono>
 #include <thread>
+#include <Eigen/Dense>
 //std::string mode_demo;
 
 
@@ -48,10 +49,9 @@ public:
         double J11 =  280.0 * 1000.0 * 1000.0;
         double J22 =  140.0 * 1000.0 * 1000.0;
         double J33 =  420.0 * 1000.0 * 1000.0;
-        J123.setValue(
-             J11, 0.0, 0.0,
-             0.0, J22, 0.0,
-             0.0, 0.0, J33);
+        J123 << J11, 0.0, 0.0,
+                0.0, J22, 0.0,
+                0.0, 0.0, J33;
         J123inv = J123.inverse();
 
         sub_t_fwd_sim = this->create_subscription<std_msgs::msg::Float64>(
@@ -98,8 +98,8 @@ private:
     //define parameters for dynamics
     //TODO: Jall, dynamic change of J, 
     // as well as CoG offset issue is a big TODO
-    tf2::Matrix3x3 J123;
-    tf2::Matrix3x3 J123inv;
+    Eigen::Matrix3d J123;
+    Eigen::Matrix3d J123inv;
 
     tf2::Quaternion attcur; // pose
     tf2::Quaternion attprv; // pose
@@ -114,7 +114,7 @@ private:
 
 
     //define parameters for simulation
-    const double Tstep_ = 0.001; // in seconds
+    double Tstep_rk; // in seconds
     const double Ttorque_ = 0.1; //same as T_callback
     //int Nstep; // number of steps for simulation, Ttorque_/Tstep_
 
@@ -125,36 +125,7 @@ private:
     bool should_pub_att; // true then publish attitude for display purpose
 
 
-    //utility function
-    tf2::Quaternion stepupdate_attitude(const tf2::Quaternion& qi, const tf2::Vector3& omeb, double dt) 
-    {
-        // Step 1: Calculate rotation angle
-        double thetab = omeb.length() * dt;
-        if(thetab == 0.0)
-            return qi;
 
-        // Step 2: Calculate rotation axis (unit vector)
-        tf2::Vector3 u = omeb.normalized();
-
-        // Step 3: Calculate rotation quaternion in the body frame
-        double half_thetab = thetab / 2.0;
-        double sin_half_thetab = std::sin(half_thetab);
-        tf2::Quaternion q_omegab(
-            u.x() * sin_half_thetab,
-            u.y() * sin_half_thetab,
-            u.z() * sin_half_thetab,
-            std::cos(half_thetab)
-        );
-        q_omegab = q_omegab.normalize();
-
-        // Step 4: Transform q_omegab to world frame
-        tf2::Quaternion q_omega_world = qi * q_omegab * qi.inverse();
-
-        // Step 5: Calculate new orientation quaternion
-        tf2::Quaternion qf = q_omega_world * qi;
-        qf = qf.normalize();
-        return qf;
-    }
 
     //utility function
     void printq(const tf2::Quaternion& q){
@@ -239,12 +210,107 @@ private:
     }
 
 
+    //utility function
+    tf2::Quaternion stepupdate_attitude(const tf2::Quaternion& qi, const tf2::Vector3& omeb, double dt) 
+    {
+        // Step 1: Calculate rotation angle
+        double thetab = omeb.length() * dt;
+        if(thetab == 0.0)
+            return qi;
+
+        // Step 2: Calculate rotation axis (unit vector)
+        tf2::Vector3 u = omeb.normalized();
+
+        // Step 3: Calculate rotation quaternion in the body frame
+        double half_thetab = thetab / 2.0;
+        double sin_half_thetab = std::sin(half_thetab);
+        tf2::Quaternion q_omegab(
+            u.x() * sin_half_thetab,
+            u.y() * sin_half_thetab,
+            u.z() * sin_half_thetab,
+            std::cos(half_thetab)
+        );
+        q_omegab = q_omegab.normalize();
+
+        // Step 4: Transform q_omegab to world frame
+        tf2::Quaternion q_omega_world = qi * q_omegab * qi.inverse();
+
+        // Step 5: Calculate new orientation quaternion
+        tf2::Quaternion qf = q_omega_world * qi;
+        qf = qf.normalize();
+        return qf;
+    }
 
     void forward_attitude_dynamics(double Tfwd_sec){
         //int Nstep = (int)(Tfwd_sec/Tstep_);
-        int Nstep = (int)(Tfwd_sec/Tstep_);
+        // Step 1: Calculate rotation angle
+        double thetab = omebcur.length();
+        if (0.5 < thetab)
+            Tstep_rk = 0.001;
+        else if(0.25 < thetab)
+            Tstep_rk = 0.05;
+        else if(0.1 < thetab)
+            Tstep_rk = 0.02;
+        else 
+            Tstep_rk = 0.01;
+
+        int Nstep = (int)(Tfwd_sec/Tstep_rk);
+        
+        Eigen::Vector3d tau_allcure(tau_allcur.x(), tau_allcur.y(), tau_allcur.z());
 
         for(int istep = 0; istep < Nstep; istep++){
+            // for current 
+            Eigen::Vector3d omega_k1(omebcur.x(), omebcur.y(), omebcur.z());
+            Eigen::Quaterniond att_k1(attcur.w(), attcur.x(), attcur.y(), attcur.z());
+        
+            // for RK4 k1 
+            Eigen::Vector3d rhs1 = J123 * omega_k1;
+            Eigen::Vector3d tau_eff = tau_allcure - omega_k1.cross(rhs1);
+            Eigen::Vector3d omega_dot_k1 = J123inv * tau_eff;
+            Eigen::Quaterniond omega_quat_k1(0, omega_k1.x(), omega_k1.y(), omega_k1.z());
+            Eigen::Quaterniond att_dot_k1;
+            att_dot_k1.coeffs() = (omega_quat_k1 * att_k1).coeffs() * 0.5;
+                
+            // for RK4 k2 
+            Eigen::Vector3d omega_k2 = omega_k1 + 0.5 * Tstep_rk  * omega_dot_k1;
+            Eigen::Quaterniond att_k2 = att_k1;
+            att_k2.coeffs() += 0.5 * Tstep_rk * att_dot_k1.coeffs();
+            Eigen::Vector3d rhs2 = J123 * omega_k2;
+            Eigen::Vector3d omega_dot_k2 = J123inv * (tau_allcure - omega_k2.cross(rhs2));
+            Eigen::Quaterniond omega_quat_k2(0, omega_k2.x(), omega_k2.y(), omega_k2.z());
+            Eigen::Quaterniond att_dot_k2;
+            att_dot_k2.coeffs() = (omega_quat_k2 * att_k2).coeffs() * 0.5;
+               
+            // for RK4 k3
+            Eigen::Vector3d omega_k3 = omega_k1 + 0.5 * Tstep_rk  * omega_dot_k2;
+            Eigen::Quaterniond att_k3 = att_k1;
+            att_k3.coeffs() += 0.5 * Tstep_rk * att_dot_k2.coeffs();
+            Eigen::Vector3d rhs3 = J123 * omega_k3;
+            Eigen::Vector3d omega_dot_k3 = J123inv * (tau_allcure - omega_k3.cross(rhs3));
+            Eigen::Quaterniond omega_quat_k3(0, omega_k3.x(), omega_k3.y(), omega_k3.z());
+            Eigen::Quaterniond att_dot_k3;
+            att_dot_k3.coeffs() = (omega_quat_k3 * att_k3).coeffs() * 0.5;
+                
+            // for RK4 k4
+            Eigen::Vector3d omega_k4 = omega_k1 + Tstep_rk * omega_dot_k3;
+            Eigen::Quaterniond att_k4 = att_k1;
+            att_k4.coeffs() += Tstep_rk * att_dot_k3.coeffs();
+            Eigen::Vector3d rhs4 = J123 * omega_k4;
+            Eigen::Vector3d omega_dot_k4 = J123inv * (tau_allcure - omega_k4.cross(rhs4));
+            Eigen::Quaterniond omega_quat_k4(0, omega_k4.x(), omega_k4.y(), omega_k4.z());
+            Eigen::Quaterniond att_dot_k4;
+            att_dot_k4.coeffs() = (omega_quat_k4 * att_k4).coeffs() * 0.5;
+              
+            // RK4 
+            omega_k1 += (Tstep_rk  / 6.0) * (omega_dot_k1 + 2 * omega_dot_k2 + 2 * omega_dot_k3 + omega_dot_k4);
+            att_k1.coeffs() += (Tstep_rk  / 6.0) * (att_dot_k1.coeffs() + 2 * att_dot_k2.coeffs() + 2 * att_dot_k3.coeffs() + att_dot_k4.coeffs());
+            att_k1.normalize();
+
+            omebcur.setValue(omega_k1.x(), omega_k1.y(), omega_k1.z());
+            attcur.setValue(att_k1.x(), att_k1.y(), att_k1.z(), att_k1.w());
+                
+
+            /*
             omebprv = omebcur;
             omedotbprv = omedotbcur; //omedotbprv not used
             attprv = attcur;
@@ -260,10 +326,10 @@ private:
             
             //3. compute moved angle 
             attcur = stepupdate_attitude(attprv,omebcur,Tstep_);
+            */
         }
-
-
     }
+
 
 
     void callback_attitude_overwrite(const geometry_msgs::msg::Quaternion::SharedPtr msg){
